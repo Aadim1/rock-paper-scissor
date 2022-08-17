666ed9b4a1513428a83d452032ea2a056393745a
Aadim Chaulagain <iamaadim5@gmail.com>
2022-08-17 15:56:28 -0700

    UI remains

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+checkWhoWon = [
+    ["It's a tie", "You loose! Paper beats rock", "You win! Rock beats scissor"],
+    ["You win! Paper beats rock", "It's a Tie", "You loose! Scissor beats paper"],
+    ["You loose! Rock beats scissor", "You win! Scissor beats paper", "It's a tie"]
+]
+
+
+let nnum = Number(prompt("How many rounds do you wanna play?: "));
+
+const getComputerChoice = () =>{
+    let val = Number(prompt("Input a number"));
+    const computerChoice = Math.floor((Math.random() * 2)) + 1;
+    alert(checkWhoWon[val][computerChoice]);
+    console.log(computerChoice);
+    console.log(val)
+}
+
+for (let i = 0; i < nnum; i++){
+    getComputerChoice();
+}
+
